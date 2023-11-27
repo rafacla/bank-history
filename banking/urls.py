8b748775a8ad74e62d5367806a1ada9aef559879
rafacla <rafacla@github.com>
2023-11-27 16:24:52 +0000

    started to deploy the Category_list view including Sortable ❤

--- banking/urls.py
+++ banking/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path('accounts/create/', views.AccountCreateView.as_view(), name='account_create'),
     path('accounts/<int:pk>/update/', views.AccountUpdateView.as_view(), name='account_update'),
     path('accounts/<int:pk>/delete/', views.AccountDeleteView.as_view(), name='account_delete'),
+    path('categories/', views.CategoryListView.as_view(), name='category_list'),
+    path('categories/<int:pk>/detail', views.CategoryDetailView.as_view(), name='category_detail'),
+    path('categories/create/', views.CategoryCreateView.as_view(), name='category_create'),
+    path('categories/<int:pk>/update/', views.CategoryUpdateView.as_view(), name='category_update'),
+    path('categories/<int:pk>/delete/', views.CategoryDeleteView.as_view(), name='category_delete'),
 ]
--- banking/views.py
+++ banking/views.py
@@ -6,14 +6,15 @@ from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
 
 
-from .models import Account
+from .models import Account, Category
 
 
 class AccountBaseView(LoginRequiredMixin, View):
     model = Account
     fields = "__all__"
     success_url = reverse_lazy("banking:account_list")
-      
+
+
 class AccountListView(AccountBaseView, ListView):
     extra_context = {"title": "Accounts"}
 
@@ -30,9 +31,17 @@ class AccountDetailView(AccountBaseView, DetailView):
 
 class AccountCreateView(AccountBaseView, CreateView):
     """View to create a new account"""
+
     extra_context = {"title": "Create Account"}
-    fields = ["name", "type", "closing_day", "due_day", "bank", "currency"]  # don't include 'user' here
-    
+    fields = [
+        "name",
+        "type",
+        "closing_day",
+        "due_day",
+        "bank",
+        "currency",
+    ]  # don't include 'user' here
+
     def form_valid(self, form):
         user = self.request.user
         form.instance.user = user
@@ -41,8 +50,68 @@ class AccountCreateView(AccountBaseView, CreateView):
 
 class AccountUpdateView(AccountBaseView, UpdateView):
     """View to update a account"""
+
     extra_context = {"title": "Update Account"}
-    fields = ["name", "type", "closing_day", "due_day", "bank", "currency"]  # don't include 'user' here
+    fields = [
+        "name",
+        "type",
+        "closing_day",
+        "due_day",
+        "bank",
+        "currency",
+    ]  # don't include 'user' here
+
 
 class AccountDeleteView(AccountBaseView, DeleteView):
     """View to delete a account"""
+
+
+class CategoryBaseView(LoginRequiredMixin, View):
+    model = Category
+    fields = "__all__"
+    success_url = reverse_lazy("banking:category_list")
+
+
+class CategoryListView(CategoryBaseView, ListView):
+    extra_context = {"title": "Categories"}
+
+    """View to list all categories.
+    Use the 'category_list' variable in the template
+    to access all Category objects"""
+
+
+class CategoryDetailView(CategoryBaseView, DetailView):
+    """View to list the details from one category.
+    Use the 'category' variable in the template to access
+    the specific category here and in the Views below"""
+
+
+class CategoryCreateView(CategoryBaseView, CreateView):
+    """View to create a new category"""
+
+    extra_context = {"title": "Create Category"}
+    fields = [
+        "name",
+        "type",
+        "nested_to"
+    ]  # don't include 'user' here
+
+    def form_valid(self, form):
+        user = self.request.user
+        form.instance.user = user
+        sort = Category
+        return super(CategoryCreateView, self).form_valid(form)
+
+
+class CategoryUpdateView(CategoryBaseView, UpdateView):
+    """View to update a category"""
+
+    extra_context = {"title": "Update Category"}
+    fields = [
+        "name",
+        "nested_to"
+    ]  # don't include 'user' here
+
+
+class CategoryDeleteView(CategoryBaseView, DeleteView):
+    """View to delete a category"""
