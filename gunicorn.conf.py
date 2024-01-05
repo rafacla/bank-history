abe7ac3471b4bd72965f47c5ad0182f8610b6cf9
rafacla <rafacla@github.com>
2024-01-05 16:48:50 +0000

    changing default port

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,5 +2,5 @@ accesslog = '-'
 access_log_format = '%(t)s [%({x-forwarded-for}i)s %(M)sms] "%(m)s %(U)s?%(q)s" %(s)s %(b)s'
 preload_app = True
 timeout = 60
-bind = '0.0.0.0:8000'
+bind = '0.0.0.0:8889'
 workers = 1
