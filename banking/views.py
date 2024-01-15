137458e40ba9f63ba70a5b593abdc046d38122a3
rafacla <rafacla@github.com>
2024-01-15 10:41:12 +0000

    made or date detection a bit smarter (also removed spaces in value field just in case)

--- banking/views.py
+++ banking/views.py
@@ -36,16 +36,35 @@ from .forms import (
 from .models import Account, Category, Transaction
 from django.shortcuts import redirect
 
-def strToDate_anyformat(format_date):
+def strToDate_anyformat(format_date, expected_format = ""):
         numbers = ''.join(re.findall(r'\d+', format_date))
-        if len(numbers) == 8:
-            d = datetime(int(numbers[:4]), int(numbers[4:6]), int(numbers[6:8]))
-        elif len(numbers) == 14:
-            d = datetime(int(numbers[:4]), int(numbers[4:6]), int(numbers[6:8]), int(numbers[8:10]), int(numbers[10:12]), int(numbers[12:14]))
-        elif len(numbers) > 14:
-            d = datetime(int(numbers[:4]), int(numbers[4:6]), int(numbers[6:8]), int(numbers[8:10]), int(numbers[10:12]), int(numbers[12:14]), microsecond=1000*int(numbers[14:]))
+        if ("/" in format_date or expected_format.upper() == "%D/%M/%Y" or expected_format.upper() == "%M/%D/%Y"):
+            #in this case, we consider the date as short_date, defaulting to %d/%m/%Y
+            if (expected_format == "" or expected_format.upper() == "%D/%M/%Y"):
+                if len(numbers) == 8 and int(numbers[2:4]) <= 12:
+                    d = datetime(int(numbers[4:8]), int(numbers[2:4]), int(numbers[:2]))
+                elif len(numbers) == 6:
+                    d = datetime(int(numbers[4:6])+2000, int(numbers[2:4]), int(numbers[:2]))
+                else:
+                    raise AssertionError(f'length not match:{format_date} or doesn\'t fit the expected format: '+expected_format)
+            else:
+                #it's in american standard:
+                if len(numbers) == 8 and int(numbers[:2]) <= 12:
+                    d = datetime(int(numbers[4:8]), int(numbers[:2]), int(numbers[2:4]))
+                elif len(numbers) == 6:
+                    d = datetime(int(numbers[4:6])+2000, int(numbers[:2]), int(numbers[2:4]))
+                else:
+                    raise AssertionError(f'length not match:{format_date} or doesn\'t fit the expected format: '+expected_format)
         else:
-            raise AssertionError(f'length not match:{format_date}')
+            #else, it's a full date:
+            if len(numbers) == 8:
+                d = datetime(int(numbers[:4]), int(numbers[4:6]), int(numbers[6:8]))
+            elif len(numbers) == 14:
+                d = datetime(int(numbers[:4]), int(numbers[4:6]), int(numbers[6:8]), int(numbers[8:10]), int(numbers[10:12]), int(numbers[12:14]))
+            elif len(numbers) > 14:
+                d = datetime(int(numbers[:4]), int(numbers[4:6]), int(numbers[6:8]), int(numbers[8:10]), int(numbers[10:12]), int(numbers[12:14]), microsecond=1000*int(numbers[14:]))
+            else:
+                raise AssertionError(f'length not match:{format_date}')
         return d
 
 
@@ -475,6 +494,7 @@ def import_csv(request):
             listOfTransactions = []
             for row in csv_reader:
                 # we try to convert the dates:
+                row["value"] = row["value"].replace(" ","")
                 row["date"] = strToDate_anyformat(row["date"])
                 # here we work in a filter to detect possible duplicated transactions:
                 duplicated_transaction = Transaction.objects.filter(account__id=row["account_id"] if "account_id" in row
