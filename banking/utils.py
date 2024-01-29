b6decf3e910c1a69272b23f5cfe6e5ce12f77932
rafacla <rafacla@github.com>
2024-01-29 17:14:10 +0000

    closes #13

--- banking/utils.py
+++ banking/utils.py
@@ -96,7 +96,7 @@ def parseCSV(import_file):
 def parsePDF(import_file):
     pdf = fitz.open(stream=import_file.read(), filetype="pdf")
     
-    def getValuesFromKey(key, below=True, right=False):
+    def getValuesFromKey(key, below=True, right=False, dx0=0, dx1=0, dy0=0, dy1=0):
         list = []
         for page in pdf:
             rectsFound = page.search_for(key)
@@ -105,7 +105,7 @@ def parsePDF(import_file):
                 y0 = rectFound[3] if below else rectFound[1]
                 x1 = rectFound[2]-rectFound[0]+x0 if right else rectFound[2]
                 y1 = rectFound[3]-rectFound[1]+y0 if below else rectFound[3]
-                rectExtract = fitz.Rect(x0, y0, x1, y1)
+                rectExtract = fitz.Rect(x0+dx0, y0+dy0, x1+dx1, y1+dy1)
                 list.append(page.get_textbox(rectExtract))
         return list
 
@@ -120,43 +120,57 @@ def parsePDF(import_file):
     if pdf[0].search_for("Com vencimento em:"):
         # For credit cards, we match the competency date with the due date of the statement
         competency_date = strToDate_anyformat(getValuesFromKey("Com vencimento em:")[0])
-        def getTables():
-            list = []
-            for page in pdf:
-                # Here we tell the script where to look in the statement:
-                vertical_lines_coordiantes =[
-                    [154, 177, 310, 345],
-                    [370, 392, 518, 560]
-                ]
-                for vlc in vertical_lines_coordiantes:
-                    tabs = page.find_tables(horizontal_strategy='lines_strict', vertical_lines=vlc)
-                    for table in tabs.tables:
-                        if table.col_count == 3:
-                            for row in table.extract():
-                                # as in itau CC statement transactions doesn't have the year, we need to consider that the year is the same of competency dates
-                                # except if the transaction date would result in a date greater then competency_date (due date of statement), in this case we should consider the past year
-                                rowDate = strToDate_anyformat(row[0]+'/'+str(competency_date.year))
-                                if isinstance(rowDate, date):
-                                    if rowDate > competency_date:
-                                        rowDate = strToDate_anyformat(row[0]+'/'+str(competency_date.year - 1))
-                                    listOfTransactions.append(
-                                        {
-                                            "select_row": False,
-                                            "value": float(str(row[2]).replace(",",".")),
-                                            "date": rowDate,
-                                            "competency_date": competency_date,
-                                            "description": str(row[1]).replace("\n"," (").replace(" .","/")+")",
-                                            "account_name": None,
-                                            "account_id": None,
-                                            "category_name": None,
-                                            "category_id": None,
-                                            "is_transfer": None,
-                                            "concilied": None,
-                                        }
-                                    )
-            return list
-
-        tableList = getTables()   
+        payment_last_statement_date = getValuesFromKey("Pagamento efetuado em ",below=False, right=True)[0]
+        payment_last_statement_value = float(getValuesFromKey("Pagamento efetuado em ",below=False, right=True, dx0=50, dx1=50)[0].replace(" ","").replace(".","").replace(",","."))*(-1)
+        
+        for page in pdf:
+            # Here we tell the script where to look in the statement:
+            vertical_lines_coordiantes =[
+                [154, 177, 310, 345],
+                [370, 392, 518, 560]
+            ]
+            for vlc in vertical_lines_coordiantes:
+                tabs = page.find_tables(horizontal_strategy='lines_strict', vertical_lines=vlc)
+                for table in tabs.tables:
+                    if table.col_count == 3:
+                        for row in table.extract():
+                            # as in itau CC statement transactions doesn't have the year, we need to consider that the year is the same of competency dates
+                            # except if the transaction date would result in a date greater then competency_date (due date of statement), in this case we should consider the past year
+                            rowDate = strToDate_anyformat(row[0]+'/'+str(competency_date.year))
+                            if isinstance(rowDate, date):
+                                if rowDate > competency_date:
+                                    rowDate = strToDate_anyformat(row[0]+'/'+str(competency_date.year - 1))
+                                listOfTransactions.append(
+                                    {
+                                        "select_row": False,
+                                        "value": float(str(row[2]).replace(",","."))*(-1),
+                                        "date": rowDate,
+                                        "competency_date": competency_date,
+                                        "description": str(row[1]).replace("\n"," (").replace(" .","/")+")",
+                                        "account_name": None,
+                                        "account_id": None,
+                                        "category_name": None,
+                                        "category_id": None,
+                                        "is_transfer": None,
+                                        "concilied": None,
+                                    }
+                                )
+        if strToDate_anyformat(payment_last_statement_date):
+            listOfTransactions.append(
+                {
+                    "select_row": False,
+                    "value": payment_last_statement_value,
+                    "date": strToDate_anyformat(payment_last_statement_date),
+                    "competency_date": None,
+                    "description": "Pagamento efetuado em "+payment_last_statement_date,
+                    "account_name": None,
+                    "account_id": None,
+                    "category_name": None,
+                    "category_id": None,
+                    "is_transfer": True,
+                    "concilied": None,
+                }
+            )
 
     return listOfTransactions
 
